1e322d0557cb7574f3c3772126dcf134b3442cbf
Crowdbotics <team@crowdbotics.com>
2020-08-27 04:46:46 +0000

    Update admin title

--- backend/mobile_27_dev_9482/urls.py
+++ backend/mobile_27_dev_9482/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "mobile 27"
-admin.site.site_title = "mobile 27 Admin Portal"
-admin.site.index_title = "mobile 27 Admin"
+admin.site.site_header = "mobile 27 aug"
+admin.site.site_title = "mobile 27 aug Admin Portal"
+admin.site.index_title = "mobile 27 aug Admin"
 
 # swagger
 api_info = openapi.Info(
